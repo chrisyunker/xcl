64af38f4aaa020366658f6837cb09b0212d5b66f
Chris Yunker <chris@yunker.io>
2014-01-15 16:05:22 -0600

    Added util function to get local ipv4 addrs

--- src/xcl_util.erl
+++ src/xcl_util.erl
@@ -14,7 +14,8 @@
          to_list/1,
          to_integer/1,
          binary_to_integer/1,
-         integer_to_binary/1]).
+         integer_to_binary/1,
+         get_local_ips/0]).
 
 -define(MAX_ID, 2147483648).
 
@@ -64,4 +65,34 @@ binary_to_integer(Bin) ->
 -spec integer_to_binary(integer()) -> binary().
 integer_to_binary(Int) ->
     list_to_binary(integer_to_list(Int)).
+
+-spec get_local_ips() -> [{integer(), integer(), integer(), integer()}].
+get_local_ips() ->
+    {ok, Ifs} = inet:getifaddrs(),
+    lists:foldl(fun process_if/2, [], Ifs).
+
+-spec process_if([tuple()], [tuple()]) -> [tuple()].
+process_if({_, If}, Acc) ->
+    {flags, Flags} = lists:keyfind(flags, 1, If),
+    case not lists:member(loopback, Flags) andalso
+        (lists:member(up, Flags) andalso
+         lists:member(running, Flags)) of
+        true ->
+            case extract_addr(If) of 
+                false -> Acc;
+                Addr -> [Addr | Acc]
+            end;
+        false ->
+            Acc
+    end;
+process_if(_, Acc) ->
+    Acc.
+
+-spec extract_addr([tuple()]) -> {integer(), integer(), integer(), integer()} | false.
+extract_addr([]) ->
+    false;
+extract_addr([{addr, {A, B, C, D}} | _]) ->
+    {A, B, C, D};
+extract_addr([_ | Tail]) ->
+    extract_addr(Tail).
 
